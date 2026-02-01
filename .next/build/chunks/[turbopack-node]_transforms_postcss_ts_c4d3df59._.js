158cd4bec49c3ab3ef51d83efb00232c3296781c
AromaChai <info.aromafnb@gmail.com>
2026-02-01 17:45:43 +0530

    add file

---
+++ .next/build/chunks/[turbopack-node]_transforms_postcss_ts_c4d3df59._.js
@@ -0,0 +1,13 @@
+module.exports = [
+"[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/Desktop/aroma-chai-landing-page/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript, async loader)", ((__turbopack_context__) => {
+
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "chunks/38452_de80f546._.js",
+  "chunks/[root-of-the-server]__855eb782._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/Desktop/aroma-chai-landing-page/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript)");
+    });
+});
+}),
+];
---
+++ .next/dev/build/chunks/[turbopack-node]_transforms_postcss_ts_e5f890b4._.js
@@ -0,0 +1,12 @@
+module.exports = [
+"[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/Desktop/aroma-chai-franchise---premium-landing-page/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript, async loader)", ((__turbopack_context__) => {
+
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "chunks/[root-of-the-server]__b7b00a5f._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/Desktop/aroma-chai-franchise---premium-landing-page/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript)");
+    });
+});
+}),
+];
